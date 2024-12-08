272add225980e687c9c0991dd6c875fd4a446647
Krishna <yadavkrishna28217@gmail.com>
2024-12-08 12:07:29 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
 
         app.listen(PORT,()=>{
-            console.log(`Server running at ${PORT}`)
+            console.log(`Server running at https://mystore-pl30.onrender.com`)
             console.log('Connected to Database')
         })
     } catch (error) {
